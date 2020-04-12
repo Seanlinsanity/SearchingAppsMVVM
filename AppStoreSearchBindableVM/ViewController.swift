3a5bd4581609fb69e12a3cc32b58684d906d68b3
Seanlinsanity <seanlin1016@gmail.com>
2020-04-12 18:05:12 +0800

    Initial Commit

---
+++ AppStoreSearchBindableVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreSearchBindableVM
+//
+//  Created by Sean on 2020/4/12.
+//  Copyright © 2020 Sean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
